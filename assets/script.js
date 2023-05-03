532fdf42de5227a0c291c06663fe3f0608a1640c
Angeles Cahill <angeles.cahill@gmail.com>
2023-05-03 11:47:06 -0400

    subiendo ingreso de presupuesto

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
-let agregarPresupuesto = document.getElementById("agregarPresupuesto");
-let presupuesto = document.getElementById("presupuesto");
-let totalPresupuesto;
-let presupuestoMonto;
+$(document).ready(function () {
+  let agregarPresupuesto = document.getElementById("agregarPresupuesto");
+  let presupuesto = document.getElementById("presupuesto");
+  let totalPresupuesto = document.getElementById("totalPresupuesto");
+  let presupuestoMonto;
+  let ingresarNuevaEntrada = [];
+  console.log(ingresarNuevaEntrada);
+  let sumaTotalPresup;
+  
 
-function showValue() {
-    let presupuesto = document.getElementById("presupuesto").value;
-    let nuevoValor = parseInt(presupuesto);
-    console.log(nuevoValor);
-    document.getElementById("totalPresupuesto").innerText = nuevoValor;
-    return nuevoValor;
-};
-let income = showValue();
-console.log(income);
+  agregarPresupuesto.addEventListener("click", function addIncome() {
+    let presupuestoNuevo = document.getElementById("presupuesto").value;
+    let nuevoValor = parseInt(presupuestoNuevo);
+    let add = ingresarNuevaEntrada.push(nuevoValor);
+    console.log(add);
+    sumaTotalPresup = ingresarNuevaEntrada.reduce((x, y) => {
+      return x + y;
+    }, 0);
+    console.log(sumaTotalPresup);
+    document.getElementById("totalPresupuesto").innerText = sumaTotalPresup;
+    document.getElementById('presupuesto') = "";
+    return sumaTotalPresup;
+  });
+
+  console.log(sumaTotalPresup);
+});
+
+
